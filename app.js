5bbf5cf911ee063211ebb7d0c576858b4051583c
Shehan Pramitha <pwshehan@gmail.com>
2019-10-02 10:04:47 +0530

    Product routes

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res, next) => {
-    res.status(200).json({
-        message: 'it works!'
-    }); 
-});
+const productsRoutes = require('./api/routes/products');
+
+app.use('/products', productsRoutes);
 
 module.exports = app;

5c25535f59175a3beebc4f571aa07d5e796396b3
Shehan Pramitha <pwshehan@gmail.com>
2019-10-03 06:15:36 +0530

    its gone

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,20 +1,46 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const orderModule = require('../models/orders');
+const Product = require('../models/products');
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Orders fetched'
+    orderModule.find()
+    .populate('product')
+    .exec()
+    .then(docs => {
+        console.log(docs);
+        if (docs.length > 0) {
+            res.status(200).json(docs);
+        } else {
+            res.status(404).json({message: 'No valid entry found'});
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
     });
 }); 
 
 router.post('/', (req, res, next) => {
-    const order = {
-        productId: req.body.productId,
-        quantity: req.body.quantity
-    };
-    res.status(201).json({
-        message: 'Order created',
-        order: order
+    const order = new orderModule({
+        _id: new mongoose.Types.ObjectId(),
+        quantity: req.body.quantity,
+        product: req.body.productId
+    });
+    order.save()
+    .then(result => {
+        console.log(result);
+        res.status(201).json(result);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
     });
 });
 
